959d4a56867d974d87c0b69f2b0e4edcf66690b4
Carl Willimott <carlwillimott@googlemail.com>
2015-07-02 20:45:41 +0100

    Add missing files.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tiptop
+Coming soon - please check back later.
+
+### Licence
+
+MIT Licence - Copyright (c) 2015 - Carl Willimott
---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var http = requere('http');
+
+var sounds = require('./sounds.json');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
