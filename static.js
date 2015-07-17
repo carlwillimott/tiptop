29e5523ba7b515e1f9a730803853a79fd20e712c
Carl Willimott <carlwillimott@googlemail.com>
2015-07-17 21:46:52 +0100

    Added a language switcher.

--- static.js
+++ static.js
@@ -2,21 +2,62 @@
 
     window.onload = function() {
 
-        var buttons = $('.button');
+        var setup = {
 
-        if (!!buttons.length) {
+            init: function() {
+                this.voices();
+                this.buttons();
+                this.switcher();
+            },
 
-            buttons.on('click', function() {
-                responsiveVoice.speak($(this).data('speech'));
-            });
+            buttons: function() {
 
-        }
+                var buttons = $('.button');
 
-        var switcher = $('#language-switcher');
+                if (!!buttons.length) {
 
-        switcher.on('change', function() {
-            responsiveVoice.setDefaultVoice($(this).val());
-        });
+                    buttons.on('click', function() {
+                        responsiveVoice.speak($(this).data('speech'));
+                    });
+
+                }
+
+            },
+
+            switcher: function() {
+
+                var switcher = $('#language-switcher');
+
+                switcher.on('change', function() {
+                    responsiveVoice.setDefaultVoice($(this).val());
+                });
+
+
+            },
+
+            voices: function() {
+
+                var voices = responsiveVoice.getVoices();
+
+                var html = '';
+
+                $.each(voices, function() {
+                    var name = $(this)[0].name;
+                    html += '<option value="' + name + '">' + name + '</option>';
+                });
+
+                var switcher = $('#language-switcher');
+
+                if (!!switcher.length) {
+                    switcher.html(html);
+                }
+
+
+            }
+
+        };
+
+        setup.init();
 
     };
 
